374628ab43945105b8fb5544865f34569d8341c0
bibiana <bcristofol@gmail.com>
2014-12-28 16:57:08 +0100

    Exercise 2.3 done

--- session2/challenge/3_array.rb
+++ session2/challenge/3_array.rb
@@ -5,4 +5,18 @@
 # "abcdefg".every_other_char  # => "aceg"
 # "".every_other_char         # => ""
 
+class String
 
+	def every_other_char
+		result = ""
+		counter = 0
+		each_char do |letter|
+			result << letter if counter % 2 == 0
+			counter += 1
+		end
+		return result
+	end
+
+end
+
+puts "".every_other_char 
