d0f00cb1f1a623c569995bd5c96418aa09a2a32c
bibiana <bcristofol@gmail.com>
2014-12-30 22:24:01 +0000

    Exercise 2.7 done

--- session2/challenge/7_array.rb
+++ session2/challenge/7_array.rb
@@ -8,3 +8,15 @@
 # alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
+
+def alternate_words(str)
+	result = []
+	counter = 0
+	str2 = str.gsub(/[!@$#%^&*()-=_+\[\]:;,.\/<>?\|]/, " ")
+	words = str2.split(/\s+/)
+	words.each do |word|
+		result << word if counter % 2 == 0
+		counter += 1
+	end
+	return result
+end
