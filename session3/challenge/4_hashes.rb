02c4741569ae2dcf463d42fedc1a389927bfca16
bibiana <bcristofol@gmail.com>
2015-01-01 23:19:11 +0000

    Exercise 3.4 done

--- session3/challenge/4_hashes.rb
+++ session3/challenge/4_hashes.rb
@@ -5,3 +5,14 @@
 # There will be no punctuation in the strings.
 # 
 # first_pos "The dog and the cat and the cow" # => { "The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7 }
+
+def first_pos(str)
+	result = Hash.new
+	counter = 0
+	words = str.split
+	words.each do |w| 
+		result[w] = counter unless result[w]
+		counter += 1
+	end
+	return result
+end
