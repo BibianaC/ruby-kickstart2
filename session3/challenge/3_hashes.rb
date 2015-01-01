d551056b4c385ab809b204f13b87d0df01db8c05
bibiana <bcristofol@gmail.com>
2015-01-01 23:07:34 +0000

    Exercise 3.3 done

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -1,4 +1,4 @@
-# Write a method that takes a string and returns an array 
+# Write a method that takes a string and returns a hash 
 # whose keys are all the downcased words in the string, 
 # and values are the number of times these words were seen.
 # 
@@ -6,3 +6,10 @@
 # 
 # Example:
 # word_count "The dog and the cat" # => { "the" => 2, "dog" => 1, "and" => 1, "cat" => 1 }
+
+def word_count(str)
+	result = Hash.new
+	words = str.downcase.split
+	words.each {|w| result[w] = words.count(w)}
+	return result
+end
